5ac1b21499479964b6833a7d59cd64230073c5af
João Costa <jdpc557@gmail.com>
2020-05-10 16:23:43 +0100

    Fix bad commit

--- core/native/src/test/scala/eu/joaocosta/minart/backend/SdlRenderLoopSpec.scala
+++
@@ -1,8 +0,0 @@
-package eu.joaocosta.minart.backend
-
-import eu.joaocosta.minart.core._
-
-class SdlRenderLoopSpec extends RenderLoopTests {
-  lazy val renderLoop: RenderLoop = SdlRenderLoop
-  lazy val renderLoopName: String = "SdlRenderLoop"
-}
