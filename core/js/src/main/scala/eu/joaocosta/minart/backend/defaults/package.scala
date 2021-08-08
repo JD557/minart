97867fba0f733fccc11813dc2dc4d3de59ec54bf
João Costa <jdpc557@gmail.com>
2021-08-08 17:06:49 +0200

    Add runtime platform detection

--- core/js/src/main/scala/eu/joaocosta/minart/backend/defaults/package.scala
+++ core/js/src/main/scala/eu/joaocosta/minart/backend/defaults/package.scala
@@ -1,9 +1,14 @@
 package eu.joaocosta.minart.backend
 
+import eu.joaocosta.minart.runtime.Platform
+
 package object defaults {
   implicit val defaultCanvas: DefaultBackend[Any, HtmlCanvas] =
     DefaultBackend.fromFunction((_) => new HtmlCanvas())
 
   implicit val defaultLoopRunner: DefaultBackend[Any, JsLoopRunner.type] =
     DefaultBackend.fromConstant(JsLoopRunner)
+
+  implicit val defaultPlatform: DefaultBackend[Any, Platform.JS.type] =
+    DefaultBackend.fromConstant(Platform.JS)
 }
