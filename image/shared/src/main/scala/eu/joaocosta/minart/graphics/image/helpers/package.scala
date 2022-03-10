68ee23d24a792be4f1e7edf1dbfde25d1aad0313
João Costa <jdpc557@gmail.com>
2022-03-10 20:19:53 +0100

    Refactor helpers

--- image/shared/src/main/scala/eu/joaocosta/minart/graphics/image/helpers/package.scala
+++ image/shared/src/main/scala/eu/joaocosta/minart/graphics/image/helpers/package.scala
@@ -6,23 +6,18 @@ package eu.joaocosta.minart.graphics.image
   */
 package object helpers {
 
-  object LazyListHelpers {
-    type ParseResult[T]   = Either[String, (LazyList[Int], T)]
-    type ParseState[E, T] = State[LazyList[Int], E, T]
+  trait HelpersF[F[_]] {
+    type ParseResult[T]   = Either[String, (F[Int], T)]
+    type ParseState[E, T] = State[F[Int], E, T]
 
     /** Skip N Bytes */
-    def skipBytes(n: Int): ParseState[Nothing, Unit] =
-      State.modify(_.drop(n))
+    def skipBytes(n: Int): ParseState[Nothing, Unit]
 
     /** Read 1 Byte */
-    val readByte: ParseState[String, Option[Int]] = State { bytes =>
-      bytes.tail -> bytes.headOption
-    }
+    val readByte: ParseState[String, Option[Int]]
 
     /** Read N Byte */
-    def readBytes(n: Int): ParseState[Nothing, Array[Int]] = State { bytes =>
-      bytes.drop(n) -> bytes.take(n).toArray
-    }
+    def readBytes(n: Int): ParseState[Nothing, Array[Int]]
 
     /** Read a String from N Bytes */
     def readString(n: Int): ParseState[Nothing, String] =
@@ -39,38 +34,31 @@ package object helpers {
     }
   }
 
-  object IteratorHelpers {
-    type ParseResult[T]   = Either[String, (Iterator[Int], T)]
-    type ParseState[E, T] = State[Iterator[Int], E, T]
+  object LazyListHelpers extends HelpersF[LazyList] {
+    def skipBytes(n: Int): ParseState[Nothing, Unit] =
+      State.modify(_.drop(n))
 
-    /** Skip N Bytes */
+    val readByte: ParseState[String, Option[Int]] = State { bytes =>
+      bytes.tail -> bytes.headOption
+    }
+
+    def readBytes(n: Int): ParseState[Nothing, Array[Int]] = State { bytes =>
+      bytes.drop(n) -> bytes.take(n).toArray
+    }
+  }
+
+  object IteratorHelpers extends HelpersF[Iterator] {
     def skipBytes(n: Int): ParseState[Nothing, Unit] =
       State.modify { s => s.drop(n); s.isEmpty; s }
 
-    /** Read 1 Byte */
     val readByte: ParseState[String, Option[Int]] = State { bytes =>
       val hd = bytes.nextOption
       bytes -> hd
     }
 
-    /** Read N Byte */
     def readBytes(n: Int): ParseState[Nothing, Array[Int]] = State { bytes =>
       val hd = bytes.take(n).toArray
       bytes -> hd
-    }
-
-    /** Read a String from N Bytes */
-    def readString(n: Int): ParseState[Nothing, String] =
-      readBytes(n).map { bytes => bytes.map(_.toChar).mkString("") }
-
-    /** Read a Integer N Bytes (Little Endian) */
-    def readLENumber(n: Int): ParseState[Nothing, Int] = readBytes(n).map { bytes =>
-      bytes.zipWithIndex.map { case (num, idx) => num.toInt << (idx * 8) }.sum
-    }
-
-    /** Read a Integer N Bytes (Big Endian) */
-    def readBENumber(n: Int): ParseState[Nothing, Int] = readBytes(n).map { bytes =>
-      bytes.reverse.zipWithIndex.map { case (num, idx) => num.toInt << (idx * 8) }.sum
     }
   }
 
