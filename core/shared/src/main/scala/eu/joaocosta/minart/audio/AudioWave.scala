ac0c6830119b7019bb773395abc0a116f67c8e6c
Joao Costa <jdpc557@gmail.com>
2022-05-21 22:25:54 +0200

    Support loading a Wave from an IndexedSeq

--- core/shared/src/main/scala/eu/joaocosta/minart/audio/AudioWave.scala
+++ core/shared/src/main/scala/eu/joaocosta/minart/audio/AudioWave.scala
@@ -22,3 +22,10 @@ case class AudioWave(
       .map(x => (math.min(math.max(-1.0, x), 1.0) * 127).toByte)
   }
 }
+
+object AudioWave {
+  def fromIndexedSeq(data: IndexedSeq[Double], sampleRate: Double): AudioWave = {
+    val duration = data.size / sampleRate
+    AudioWave(t => data.applyOrElse((t * sampleRate).toInt, _ => 0.0), duration)
+  }
+}
