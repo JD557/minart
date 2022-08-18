ad884323c4a589fb3910a8cf82c79f1b38c848c1
João Costa <jdpc557@gmail.com>
2022-08-18 16:33:00 +0100

    Simplify image writer tests

--- image/shared/src/test/scala/eu/joaocosta/minart/graphics/image/ImageWriterSpec.scala
+++ image/shared/src/test/scala/eu/joaocosta/minart/graphics/image/ImageWriterSpec.scala
@@ -9,24 +9,23 @@ import eu.joaocosta.minart.runtime._
 
 object ImageWriterSpec extends BasicTestSuite {
 
+  def roundtripTest(baseResource: Resource, imageFormat: ImageLoader with ImageWriter) = {
+    val originalImage = baseResource.withInputStream(is => imageFormat.loadImage(is).toOption.get).get
+    val os            = new ByteArrayOutputStream()
+    imageFormat.storeImage(originalImage, os)
+    val is       = new ByteArrayInputStream(os.toByteArray)
+    val newImage = imageFormat.loadImage(is).toOption.get
+    assert(newImage.getPixels().map(_.toVector) == originalImage.getPixels().map(_.toVector))
+  }
+
   // Can't load resources in JS tests
   if (Platform() != Platform.JS) {
     test("Write a PPM image") {
-      val originalImage = Image.loadPpmImage(Resource("scala.ppm")).get
-      val os            = new ByteArrayOutputStream()
-      ppm.PpmImageFormat.defaultFormat.storeImage(originalImage, os)
-      val is       = new ByteArrayInputStream(os.toByteArray)
-      val newImage = ppm.PpmImageFormat.defaultFormat.loadImage(is).toOption.get
-      assert(newImage.getPixels().map(_.toVector) == originalImage.getPixels().map(_.toVector))
+      roundtripTest(Resource("scala.ppm"), ppm.PpmImageFormat.defaultFormat)
     }
 
     test("Write a BMP image") {
-      val originalImage = Image.loadBmpImage(Resource("scala.bmp")).get
-      val os            = new ByteArrayOutputStream()
-      bmp.BmpImageFormat.defaultFormat.storeImage(originalImage, os)
-      val is       = new ByteArrayInputStream(os.toByteArray)
-      val newImage = bmp.BmpImageFormat.defaultFormat.loadImage(is).toOption.get
-      assert(newImage.getPixels().map(_.toVector) == originalImage.getPixels().map(_.toVector))
+      roundtripTest(Resource("scala.bmp"), bmp.BmpImageFormat.defaultFormat)
     }
   }
 }
