c95fcbca232b4895729b24d221911782593be785
João Costa <jdpc557@gmail.com>
2023-02-20 18:09:27 +0000

    Default to 16-bit on Wav output

--- sound/shared/src/main/scala/eu/joaocosta/minart/audio/sound/wav/WavAudioWriter.scala
+++ sound/shared/src/main/scala/eu/joaocosta/minart/audio/sound/wav/WavAudioWriter.scala
@@ -17,17 +17,30 @@ trait WavAudioWriter[ByteSeq] extends AudioClipWriter {
 
   val sampleRate = 44100
   val chunkSize  = 128
+  def bitRate: Int
+  require(Set(8, 16, 32).contains(bitRate))
 
   import byteWriter._
 
+  private def convertSample(x: Double): List[Int] = bitRate match {
+    case 8 =>
+      List((math.min(math.max(-1.0, x), 1.0) * Byte.MaxValue).toInt + 127)
+    case 16 =>
+      val short = (math.min(math.max(-1.0, x), 1.0) * Short.MaxValue).toInt
+      List(short & 0xff, (short >> 8) & 0xff)
+    case 32 =>
+      val int = (math.min(math.max(-1.0, x), 1.0) * Int.MaxValue).toInt
+      List(int & 0xff, (int >> 8) & 0xff, (int >> 16) & 0xff, (int >> 24) & 0xff)
+  }
+
   @tailrec
   private def storeData(
-      iterator: Iterator[Seq[Byte]],
+      iterator: Iterator[Seq[Double]],
       acc: ByteStreamState[String] = emptyStream
   ): ByteStreamState[String] = {
     if (!iterator.hasNext) acc
     else {
-      val chunk = iterator.next().map(_.toInt + 127)
+      val chunk = iterator.next().flatMap(convertSample)
       storeData(iterator, acc.flatMap(_ => writeBytes(chunk)))
     }
   }
@@ -35,8 +48,8 @@ trait WavAudioWriter[ByteSeq] extends AudioClipWriter {
   private def storeDataChunk(clip: AudioClip): ByteStreamState[String] =
     for {
       _ <- writeString("data")
-      _ <- writeLENumber(clip.numSamples(sampleRate), 4)
-      _ <- storeData(clip.byteIterator(sampleRate).grouped(chunkSize))
+      _ <- writeLENumber(clip.numSamples(sampleRate) * bitRate / 8, 4)
+      _ <- storeData(clip.iterator(sampleRate).grouped(chunkSize))
     } yield ()
 
   private def storeFmtChunk(clip: AudioClip): ByteStreamState[String] =
@@ -46,14 +59,14 @@ trait WavAudioWriter[ByteSeq] extends AudioClipWriter {
       _ <- writeLENumber(1, 2)
       _ <- writeLENumber(1, 2)
       _ <- writeLENumber(sampleRate, 4)
-      _ <- writeLENumber(sampleRate, 4)
+      _ <- writeLENumber(sampleRate * bitRate / 8, 4)
       _ <- writeLENumber(1, 2)
-      _ <- writeLENumber(8, 2)
+      _ <- writeLENumber(bitRate, 2)
     } yield ()
 
   private def storeRiffHeader(clip: AudioClip): ByteStreamState[String] = for {
     _ <- writeString("RIFF")
-    _ <- writeLENumber(36 + clip.numSamples(sampleRate), 4)
+    _ <- writeLENumber(36 + clip.numSamples(sampleRate) * bitRate / 8, 4)
     _ <- writeString("WAVE")
   } yield ()
 
