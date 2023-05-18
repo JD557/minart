aa3d4206d042ec8d78c0a78f9a357424709f78f4
João Costa <jdpc557@gmail.com>
2023-05-18 16:05:13 +0200

    Add QOA reader tests

--- sound/shared/src/test/scala/eu/joaocosta/minart/audio/sound/AudioClipReaderSpec.scala
+++ sound/shared/src/test/scala/eu/joaocosta/minart/audio/sound/AudioClipReaderSpec.scala
@@ -10,6 +10,19 @@ import eu.joaocosta.minart.runtime._
 
 object AudioClipReaderSpec extends BasicTestSuite {
 
+  def approx(x: Double, y: Double, epsilon: Double = 0.01): Boolean =
+    x >= y - epsilon && x <= y + epsilon
+
+  def similarClip(results: List[AudioClip], epsilon: Double = 0.01): Unit = {
+    results.sliding(2).foreach {
+      case clip1 :: clip2 :: _ =>
+        val zippedSamples = Sampler.sampleClip(clip1, 44100).zip(Sampler.sampleClip(clip2, 44100)).toList
+        val mse           = zippedSamples.map { case (x, y) => (x - y) * (x - y) }.sum / zippedSamples.size
+        assert(mse <= epsilon)
+      case _ => ()
+    }
+  }
+
   def sameClip(results: List[AudioClip]): Unit = {
     results.sliding(2).foreach {
       case clip1 :: clip2 :: _ =>
@@ -19,7 +32,7 @@ object AudioClipReaderSpec extends BasicTestSuite {
   }
 
   def testDuration(clip: AudioClip, duration: Double, epsilon: Double = 0.01): Unit = {
-    assert(clip.duration >= duration - epsilon && clip.duration <= duration + epsilon)
+    assert(approx(clip.duration, duration, epsilon))
   }
 
   // Can't load resources in JS tests
@@ -34,11 +47,26 @@ object AudioClipReaderSpec extends BasicTestSuite {
       testDuration(clip.get, 0.3975)
     }
 
-    test("Load the same data from different formats") {
+    test("Load a QOA file") {
+      val clip = Sound.loadQoaClip(Resource("sample.qoa"))
+      testDuration(clip.get, 0.3975)
+    }
+
+    test("Load the same data from different lossless formats") {
       sameClip(
         List(
           Sound.loadWavClip(Resource("sample-32bit.wav")).get,
           Sound.loadAiffClip(Resource("sample-32bit.aiff")).get
+        )
+      )
+    }
+
+    test("Load the similar data from different formats") {
+      similarClip(
+        List(
+          Sound.loadWavClip(Resource("sample-32bit.wav")).get,
+          Sound.loadAiffClip(Resource("sample-32bit.aiff")).get,
+          Sound.loadQoaClip(Resource("sample.qoa")).get
         )
       )
     }
