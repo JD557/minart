5cf2df2a2ca61f2f0714bcd847b2222a16330989
João Costa <jdpc557@gmail.com>
2023-08-25 21:29:10 +0200

    Add Plane#overlay and SurfaceView#overlay

--- core/shared/src/main/scala/eu/joaocosta/minart/graphics/BlendMode.scala
+++ core/shared/src/main/scala/eu/joaocosta/minart/graphics/BlendMode.scala
@@ -1,20 +1,45 @@
 package eu.joaocosta.minart.graphics
 
 /** Blend strategy to be used by the blitter */
-sealed trait BlendMode
+sealed trait BlendMode {
+  def blend(src: => Color, dst: => Color): Color
+}
 object BlendMode {
 
   /** Simply copy the pixel values */
-  case object Copy extends BlendMode
+  case object Copy extends BlendMode {
+    def blend(src: => Color, dst: => Color): Color =
+      src
+  }
 
   /** Copy all pixels except the ones with the same value as the mask */
-  final case class ColorMask(mask: Color) extends BlendMode
+  final case class ColorMask(mask: Color) extends BlendMode {
+    def blend(src: => Color, dst: => Color): Color = {
+      val colorSource = src
+      if (colorSource != mask) src else dst
+    }
+  }
 
   /** Copy all pixels with alpha greater than the provided value */
-  final case class AlphaTest(alpha: Int) extends BlendMode
+  final case class AlphaTest(alpha: Int) extends BlendMode {
+    def blend(src: => Color, dst: => Color): Color = {
+      val colorSource = src
+      if (colorSource.a > alpha) src else dst
+    }
+  }
 
   /** Blends the surfaces using weighted adition: dstColor * (1-srcAlpha) + srcColor
     *  This behaves as normal alpha blending if the source uses premultiplied alpha.
     */
-  case object AlphaAdd extends BlendMode
+  case object AlphaAdd extends BlendMode {
+    def blend(src: => Color, dst: => Color): Color = {
+      val colorSource = src
+      val colorDest   = dst
+      Color(
+        Math.min((colorDest.r * (255 - colorSource.a)) / 255 + colorSource.r, 255),
+        Math.min((colorDest.g * (255 - colorSource.a)) / 255 + colorSource.g, 255),
+        Math.min((colorDest.b * (255 - colorSource.a)) / 255 + colorSource.b, 255)
+      )
+    }
+  }
 }
