756b8a7834cf937e119de129d5d13275efa30c04
João Costa <jdpc557@gmail.com>
2023-10-07 18:17:39 +0200

    Make AllSubsystems a cake

---
+++ core/shared/src/main/scala/eu/joaocosta/minart/backend/subsystem/AudioPlayerSubsystem.scala
@@ -0,0 +1,7 @@
+package eu.joaocosta.minart.backend.subsystem
+
+import eu.joaocosta.minart.audio.AudioPlayer
+
+/** Subsystem cake with an AudioPlayer
+  */
+trait AudioPlayerSubsystem(val audioPlayer: AudioPlayer)
---
+++ core/shared/src/main/scala/eu/joaocosta/minart/backend/subsystem/CanvasSubsystem.scala
@@ -0,0 +1,7 @@
+package eu.joaocosta.minart.backend.subsystem
+
+import eu.joaocosta.minart.graphics.Canvas
+
+/** Subsystem cake with a Canvas
+  */
+trait CanvasSubsystem(val canvas: Canvas)
